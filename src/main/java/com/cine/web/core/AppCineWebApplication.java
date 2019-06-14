448019b98819ea457738a47e2fd14ca73bba06ac
Marcos Porturas <mportura@everis.com>
2019-06-13 19:17:05 -0500

    Initial Commit

---
+++ src/main/java/com/cine/web/core/AppCineWebApplication.java
@@ -0,0 +1,13 @@
+package com.cine.web.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCineWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCineWebApplication.class, args);
+	}
+
+}
